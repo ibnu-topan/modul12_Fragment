2b3cd431dfaee26e6a073beda44e7f2f515d7a60
ibnu-topan <ibnutopanid@gmail.com>
2021-06-19 06:17:43 +0700

    create layout "fragment_second.xml"

--- app/src/main/java/com/ibnu/modul12_fragment/FirstFragment.kt
+++ app/src/main/java/com/ibnu/modul12_fragment/FirstFragment.kt
@@ -1,4 +1,30 @@
 package com.ibnu.modul12_fragment
 
-class FirstFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+class FirstFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val v = inflater.inflate(R.layout.fragment_first, container, false)
+        val context = activity as AppCompatActivity
+        val btnNavigate: Button = v.findViewById(R.id.btnNavigate)
+        btnNavigate.setOnClickListener {
+            context.replaceFragment(SecondFragment())
+        }
+        return v
+    }
+    fun AppCompatActivity.replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val transaction = fragmentManager.beginTransaction()
+        transaction.replace(R.id.host,fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
 }
