725538785cd46429e655df3f4ab457b03d4909eb
ibnu-topan <ibnutopanid@gmail.com>
2021-06-19 06:12:56 +0700

    create function replace fragment

--- app/src/main/java/com/ibnu/modul12_fragment/MainActivity.kt
+++ app/src/main/java/com/ibnu/modul12_fragment/MainActivity.kt
@@ -2,10 +2,19 @@ package com.ibnu.modul12_fragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        replaceFragment(FirstFragment())
+    }
+    fun AppCompatActivity.replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val transaction = fragmentManager.beginTransaction()
+        transaction.replace(R.id.host,fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
